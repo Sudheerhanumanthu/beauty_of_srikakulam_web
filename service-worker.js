8700cb1abe5f1c38ab128400e9126025228075b4
Sudheerhanumanthu <sudheerhanumanthu31@gmail.com>
2025-07-09 15:45:11 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ self.addEventListener('install', (event) => {
       return cache.addAll([
         'index.html',
         'manifest.json',
-        'icon.png'
+        'Lamborghini-Wallpaper.jpg'
       ]);
     })
   );
